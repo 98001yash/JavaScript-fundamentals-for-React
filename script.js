57374169c50f99e2a9e2141e641a67364bdbaa5b
98001/yash <yashchauhan.gaya@gmail.com>
2025-04-23 16:51:52 +0530

    implemented some basic functionality of the javascript

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-const x = "Yash"
-console.log(x);
+// const x = "Yash"
+// console.log(x);
+
+
+
+// creating an object in the javascript
+// const obj = {
+//     name: "Yash",
+//     age: 20,
+
+// };
+
+// console.log(obj.name);
+// console.log(obj.age);
+
+
+
+// to change the data types(String) of the variable
+const x = 1;
+const y = String(x);
+
+const z = Boolean(x)
+console.log(z)
+console.log(y)
+console.log(typeof y)
+
+
+// Arithmetic operations
